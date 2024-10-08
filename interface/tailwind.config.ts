3549543fb556663746d1584bd9f430d8a5827877
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-10-08 15:15:04 +0200

    Tailwind configuration. Color Setup implementation, shades and mapping added.

--- interface/tailwind.config.ts
+++ interface/tailwind.config.ts
@@ -1,4 +1,33 @@
 import type { Config } from "tailwindcss";
+import { createThemes } from "tw-colors";
+import colors from "tailwindcss/colors";
+
+const baseColors = ["gray", "yellow", "red", "green", "blue", "indigo", "purple", "pink"];
+
+const shadeMapping = {
+  "50":"900",
+  "100":"800",
+  "200":"700",
+  "300":"600",
+  "400":"500",
+  "500":"400",
+  "600":"300",
+  "700":"200",
+  "800":"100",
+  "900":"50"
+}
+
+const colorMapping = (color:any, mapping:any, invert = false) => {
+  const theme:any = {};
+  baseColors.forEach((color) => {
+    theme[color] = {};
+    Object.entries(mapping).forEach(([key, value]:any) => {
+      const shadeKey = invert ? value : key;
+      theme[color][key] = colors[color][shadeKey];
+    });
+  });
+  return theme;
+}
 
 const config: Config = {
   content: [
